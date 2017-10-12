6cc68edd74cbbb9f6e91f337ca6352016b1737ff
jbrueda <jbrueda@outlook.com>
2017-10-12 11:17:14 -0300

    Intentando...

---
+++ src/Musico.rb
@@ -0,0 +1,13 @@
+class Musico
+	# Constructor
+	def initialize(esSolista,habilidadBase)
+		@esSolista     = esSolista
+		@habilidadBase = habilidadBase
+	end
+
+	# Metodos de instancia
+	def enGrupo
+		return (esSolista)
+	end
+
+end
